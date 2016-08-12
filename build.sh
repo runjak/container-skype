993adb2d994884ccf3ef9574e9e2285eac36c7b0
Jakob Runge <sicarius@g4t3.de>
2016-08-12 12:08:13 +0200

    Adjust {build,run}.sh
    - Docker image should go by the name runjak/skype.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t runjak/docker-skype .
+docker build -t runjak/skype .
--- run.sh
+++ run.sh
@@ -31,4 +31,4 @@ docker run -it \
   --memory 512mb \
   --cap-add=SYS_ADMIN \
   --rm \
-  runjak/docker-skype:latest
+  runjak/skype:latest
